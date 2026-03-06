7a556d1d30e401cead12357f3ec49c9cb37759b1
Cody Fincher <cody@litestar.dev>
2026-03-06 23:08:38 +0000

    chore(lint): fix typing.py missing import strictness

--- src/py/mogemma/_typing.py
+++ src/py/mogemma/_typing.py
@@ -19,14 +19,14 @@ _TokenizerImpl: Any | None = None
 trace: Any | None
 
 try:
-    import sentencepiece as _sp  # type: ignore[import-untyped]
+    import sentencepiece as _sp  # type: ignore[import-untyped,import-not-found] # pyright: ignore[reportMissingImports]
 
     _SPProcessorImpl = _sp.SentencePieceProcessor
 except ModuleNotFoundError:
     _SPProcessorImpl = None
 
 try:
-    from opentelemetry import trace as _trace
+    from opentelemetry import trace as _trace  # type: ignore[import-not-found] # pyright: ignore[reportMissingImports]
 except ModuleNotFoundError:
     trace = None
 else:
